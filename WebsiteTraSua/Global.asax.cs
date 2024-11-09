8655cc3e9522fc5af74e6216022c3b8d7e6b2550
Nguyen Gia Huy <vcs@z4hyrei.dev>
2024-11-09 15:21:41 +0000

    treewide: Migrate to Visual Studio (2022)

--- WebsiteTraSua/Global.asax.cs
+++ WebsiteTraSua/Global.asax.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace WebsiteTraSua
@@ -12,6 +10,9 @@ namespace WebsiteTraSua
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
