9b277e1c87ee089cfa8e815e3cb95ef99ffd19c9
Nguyen Gia Huy <vcs@z4hyrei.dev>
2024-11-10 10:48:12 +0000

    frontend: Add 'Admin' page, standardize codes

---
+++ WebsiteTraSua/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+using System.Web.Mvc;
+
+namespace WebsiteTraSua.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        // GET: Admin/TrangChu
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+    }
+}
